def863d298662e569a15f4cbc8c34402cb3a1df1
Arta <artakh10@gmail.com>
2025-01-15 21:50:37 +0330

    Module
    An initial introduction to modules

---
+++ Basics/module.py
@@ -0,0 +1,15 @@
+'''
+Soecifically for python beginners
+Author: Arta Khosravi
+Last edit: Jan. 2025
+'''
+#importing a module
+#random number :
+import random
+from random import randrange
+#for installing a different package/module, use !pip (pip in terminal) install #module name
+
+
+num = random.random()
+num_2 = randrange(10,20) #prints a random int number between 10 to 20
+print(num, num_2)
